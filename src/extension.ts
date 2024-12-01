991cdce2fece24c5056dc821a6d6b9a6d20f0b8d
Lucas <lucasnrgaard@gmail.com>
2024-12-01 14:37:08 +0100

    dump

--- src/extension.ts
+++ src/extension.ts
@@ -1,4 +1,4 @@
-import { ExtensionContext, commands, window, workspace, Uri } from "vscode";
+import { ExtensionContext, commands, window, workspace, Uri, version } from "vscode";
 import {
   extractSchema,
   getConfiguration,
@@ -20,142 +20,29 @@ export function activate(context: ExtensionContext) {
 
       const baseUri = _workspace.uri;
 
-      const releases = await getReleases(baseUri, releaseList);
+      try {
+        const schemaList = await fetch("https://raw.githubusercontent.com/luxass/vscode-schemas/refs/heads/main/schemas/" + version + "/schema-list.json")
 
-      if (!releases) {
-        return;
-      }
+        const schemaListJson = await schemaList.json();
 
-      const release = await window.showQuickPick(
-        releases.map((release) => release.name),
-        {
-          title: "Pick a release"
+        if (!Array.isArray(schemaListJson)) {
+          throw new Error("Invalid schema list.");
         }
-      );
 
-      const releaseUri = releases.find((r) => r.name === release)?.url;
-      console.log(releaseUri);
-
-      if (!release || !releaseUri) {
-        return;
-      }
-
-      const root = await getSchemaList(baseUri, releaseUri);
-
-      if (!root) {
-        return;
-      }
-
-      if (!root.schemas) {
-        window.showErrorMessage("No schemas found in list.");
-        return;
-      }
-
-      if (!Array.isArray(root.schemas)) {
-        window.showErrorMessage("Schemas is a non-array.");
-        return;
-      }
-
-      let output =
-        outputPath ||
-        (await window.showInputBox({
-          title: "Output path",
-          value: "./"
-        })) ||
-        "./";
-
-      if (!output) {
-        output = "./";
-      }
-
-      await workspace.fs.createDirectory(Uri.joinPath(baseUri, output));
-
-      await Promise.all([
-        ...(root.schemas || []).map(async (schema) =>
-          extractSchema(baseUri, output, schema)
-        ),
-        ...(root.externalSchemas || []).map(async (schema) =>
-          extractSchema(baseUri, output, schema)
-        )
-      ]);
-      window.showInformationMessage("Schemas extracted.");
-    })
-  );
-
-  context.subscriptions.push(
-    commands.registerCommand("schema-extractor.extract-one", async () => {
-      const { releaseList, outputPath } = getConfiguration();
-
-      const _workspace = await getWorkspace();
-
-      if (!_workspace) {
-        return;
-      }
-
-      const baseUri = _workspace.uri;
-
-      const releases = await getReleases(baseUri, releaseList);
-
-      if (!releases) {
-        return;
-      }
-
-      const release = await window.showQuickPick(
-        releases.map((release) => release.name),
-        {
-          title: "Pick a release"
+        for (const schema of schemaListJson) {
+          console.log("Extracting schema:", schema);
+          await extractSchema(baseUri, outputPath, schema);
         }
-      );
 
-      const releaseUri = releases.find((r) => r.name === release)?.url;
-      console.log(releaseUri);
+        console.log("Schema extraction completed.");
 
-      if (!release || !releaseUri) {
-        return;
+      } catch (e) {
+        console.log("version:", version);
+        console.error(e);
+        window.showErrorMessage("Something went wrong.");
       }
 
-      const schemas = await getSchemaList(baseUri, releaseUri);
 
-      if (!schemas) {
-        return;
-      }
-
-      if (!schemas.schemas) {
-        window.showErrorMessage("No schemas found in list.");
-        return;
-      }
-
-      if (!Array.isArray(schemas.schemas)) {
-        window.showErrorMessage("Schemas is a non-array.");
-        return;
-      }
-
-      const schema = await window.showQuickPick(
-        schemas.schemas.concat(schemas.externalSchemas),
-        {
-          title: "Pick a schema"
-        }
-      );
-
-      if (!schema) {
-        return;
-      }
-
-      let output =
-        outputPath ||
-        (await window.showInputBox({
-          title: "Output path",
-          value: "./"
-        })) ||
-        "./";
-
-      if (!output) {
-        output = "./";
-      }
-
-      await workspace.fs.createDirectory(Uri.joinPath(baseUri, output));
-
-      await extractSchema(baseUri, output, schema);
     })
   );
 }
